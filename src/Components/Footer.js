c78ee735d60833d852c513e773829adec9172d4d
yeganeh <ytaghavi@yahoo.com>
2022-02-06 10:33:04 -0800

    add Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+//import TeamsIcon from '../svgs/microsoftTeamsIcon'
+import React from 'react'
+import {FaDiscord, FaInstagram, FaYoutube} from 'react-icons/fa';
+import {SiMicrosoftteams} from 'react-icons/si';
+
+
+export default function Footer(props)
+{
+    return(
+        <div className={`w-screen h-[100px] p-10 flex text-white ${!props.className ? "" : props.className}`}>
+            <div className="flex flex-col w-1/2 items-center justify-center subheading-text text-xs">
+                <a href="mailto:ytaghavi@yahoo.com">Email Ellie (Co-Leader)</a>
+                <a href="mailto:ytaghavi@yahoo.com">Email Spencer (Co-Leader)</a>
+            </div>
+            <div className="flex flex-row w-1/2 items-center justify-center">
+                <a href="https://example.com"><FaDiscord className="w-6 h-6 mx-2"/></a>
+                <a href="https://example.com"><FaInstagram className="w-6 h-6 mx-2"/></a>
+                <a href="https://example.com"><FaYoutube className="w-6 h-6 mx-2"/></a>
+                <a href="https://exmaple.com"><SiMicrosoftteams className="w-6 h-6 mx-2"/></a>
+            </div>
+        </div>
+    )
+}
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,13 @@
 import Countdown from "./Components/Countdown"
 import TextBox from "./Components/TextBox"
+import Footer from './Components/Footer'
 
 //fetch the time for meeting countdown from a DB. The time will be set in the DB using a discord bot
 
 export default function(props){
     return (
         <div>
-            <div className="bg-green-50 h-screen flex items-center text-center justify-center">
+            <div className="bg-green-50 h-[90vh] flex items-center text-center justify-center">
                 <div className="flex flex-col items-center w-full">
                     <h1 className="title-text text-[65px] sm:title-text md:mb-4 lg:mb-10">
                         <span>HSS</span><span className="block lg:inline"> Engineering</span><span> Club</span>
@@ -17,11 +18,15 @@ export default function(props){
                     </div>
                 </div>
             </div>
-            <div>
+            <div className="bg-black py-5">
+                <TextBox className="my-3 mb-10" title="What Is Engineering Club?">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </TextBox>
                 <TextBox className="my-3" title="What Is Engineering Club?">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </TextBox>
             </div>
+            <Footer className="bg-blue-600"/>
         </div>
     )
 }
