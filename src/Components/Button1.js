fb807d588f3756540593be0562a860fe9c05587e
yeganeh <ytaghavi@yahoo.com>
2022-02-13 15:03:13 -0800

    add styled button1

---
+++ src/Components/Button1.js
@@ -0,0 +1,11 @@
+import Button from "./Button"
+
+export default function(props){
+    return(
+        <Button gradient1="green-300" gradient2="blue-500" gradient="green-300"
+            className="flex flex-row text-center items-center lg:w-52 lg:h-14">
+            <a href={props.href}>{props.icon}</a>    
+            <span>{props.text}</span>
+        </Button>
+    )
+}
