a7be833519df046ca717a706144f700b50275b1f
yeganeh <ytaghavi@yahoo.com>
2022-02-14 13:32:07 -0800

    make workshop page responsive

--- src/Workshop.js
+++ src/Workshop.js
@@ -2,8 +2,8 @@ export default function Workshop(props){
     return(
         <div className="bg-white py-20 px-10">
             <h1 className="heading-text text-3xl mb-5 text-cyan-500">The workshop</h1>
-            <p className="text-blakc mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
-            <div className="grid grid-cols-2 gap-5">
+            <p className="text-black mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+            <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-5">
                 <img className="border-sm rounded-md" src="https://source.unsplash.com/collection/483251/" alt="image of the location of Handsworth"/>
                 <img className="border-sm rounded-md" src="https://source.unsplash.com/collection/483251/" alt="image of the location of Handsworth"/>
                 <img className="border-sm rounded-md" src="https://source.unsplash.com/collection/483251/" alt="image of the location of Handsworth"/>
