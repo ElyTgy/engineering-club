84ceb74a43107521e3ad40d5ee977e0ac0f8966f
yeganeh <ytaghavi@yahoo.com>
2022-02-26 13:05:27 -0800

    scale button text for Button1 Component

--- src/Components/Button1.js
+++ src/Components/Button1.js
@@ -1,11 +1,11 @@
 import Button from "./Button"
 
-export default function(props){
+export default function Button1(props){
     return(
         <Button gradient1="green-300" gradient2="blue-500" gradient="green-300"
             className={`flex flex-row text-center items-center lg:w-52 lg:h-14 ${props.className ? props.className : ""}`}>
             <a href={props.href}>{props.icon}</a>    
-            <span className="m-auto">{props.text}</span>
+            <span className="m-auto text-xs sm:text-base lg:text-lg xl:text-lg">{props.text}</span>
         </Button>
     )
 }
