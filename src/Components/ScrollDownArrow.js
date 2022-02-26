6713d169451cbef3003cc6674d75e0c27feffc58
yeganeh <ytaghavi@yahoo.com>
2022-02-26 13:24:33 -0800

    center arrow

--- src/Components/ScrollDownArrow.js
+++ src/Components/ScrollDownArrow.js
@@ -3,10 +3,8 @@ import '.././test.css'
 
 export default function Arrow(props){
     return(
-        <a href={props.href} className="scroll-smooth cursor-pointer">
-            <div className="scroll-smooth animate-bounce bg-white p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center bottom-0 absolute">
-                <BsArrowDown className="scroll-smooth inline"/> 
-            </div>
+        <a href={props.href} className="cursor-pointer scroll-smooth animate-bounce bg-white p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full absolute bottom-0">
+            <BsArrowDown className="scroll-smooth inline"/> 
         </a>
     )
 }
--- src/Join.js
+++ src/Join.js
@@ -21,11 +21,11 @@ export default function Join(props){
                             <Button1 text="Join on Teams" href="https://example.com" icon={<SiMicrosoftteams className="w-6 h-6 mx-3"/>}/>
                         </div>
                     </div>
-                <div class="custom-shape-divider-bottom-1644785508">
-                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                        <path d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z" class="shape-fill"></path>
-                    </svg>
-                </div>
+                    <div class="custom-shape-divider-bottom-1644785508">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z" class="shape-fill"></path>
+                        </svg>
+                    </div>
                     <Arrow href="#what-should-i-do-for-the-first-meeting"/>
                 </div>
             </div>
