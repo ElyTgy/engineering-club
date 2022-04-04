ae55e883e82e31df0d5236a3237b2149e425a8e5
Ellie <ytaghavi@yahoo.com>
2022-04-04 00:52:46 -0700

    make contact page responsive

--- src/Components/LeaderDesc.js
+++ src/Components/LeaderDesc.js
@@ -3,7 +3,7 @@ export default function LeaderDesc(props){
         <div className={`mb-10 m-auto ${props.className ? props.className : ""}`}>
             <h2 className="subheading-text text-cyan-600 mb-3">{`${props.name} (${props.role})`}</h2>
             <p className="body-text-2">{props.children}</p>
-            <div className="flex flex-row space-between justify-around mt-5 title-text text-base">
+            <div className="flex flex-row space-between justify-around mt-5 title-text text-base md:text-xl">
                 {props.buttons}
             </div>
         </div>
--- src/Contact.js
+++ src/Contact.js
@@ -8,20 +8,22 @@ export default function Contact(props){
     <div className="bg-white py-20 px-10">
         <div className="max-w-5xl m-auto">
             <h1 className="heading-text text-center text-3xl mb-8 text-cyan-500">Contact Us</h1>
-            <LeaderDesc name="Spencer Grey" role="Leader">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-            </LeaderDesc>
-            
-            <LeaderDesc name="Ellie Taghavi" role="Leader"
-                buttons={
-                    [
-                        <CopyText text="discord" copyText="elytgy#4725"/>,
-                        <a className="link" href="https://github.com/elytgy" target="_blank">github</a>,
-                        <a className="link" href="https://www.polywork.com/elytgy" target="_blank">polywork</a>,
-                    ]
-                }>
-                Hi! I'm Ellie. I'm really interested in software and how it can be used alongside projects at our club. I have been programming for about two years now; with most of my focus begin on full-stack development and competetive programming. Every now and then I also make stuff with micro-controllers. I also enjoy going to hackathons! If you're also a dev or interested in engineering, then reach out to me :)
-            </LeaderDesc>
+            <div className="mb-32">
+                <LeaderDesc name="Spencer Grey" role="Leader">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                </LeaderDesc>
+                
+                <LeaderDesc name="Ellie Taghavi" role="Leader"
+                    buttons={
+                        [
+                            <CopyText text="discord" copyText="elytgy#4725"/>,
+                            <a className="link" href="https://github.com/elytgy" target="_blank">github</a>,
+                            <a className="link" href="https://www.polywork.com/elytgy" target="_blank">polywork</a>,
+                        ]
+                    }>
+                    Hi! I'm Ellie. I'm really interested in software and how it can be used alongside projects at our club. I have been programming for about two years now; with most of my focus begin on full-stack development and competetive programming. Every now and then I also make stuff with micro-controllers. I also enjoy going to hackathons! If you're also a dev or interested in engineering, then reach out to me :)
+                </LeaderDesc>
+            </div>
             <div className="md:w-[1/2]">
                 <h1 className="mt-10 subheading-text text-center text-2xl mb-8 text-cyan-600">Got a Question?</h1>
                 <form action="https://formspree.io/f/xgedkvnn"
